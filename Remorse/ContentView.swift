940f40d79849ca6d5b8faeeb398f2abcf9efc45d
ajioka-fumito <ajioka.ds@gmail.com>
2020-02-29 21:20:16 +0900

    Initial Commit

---
+++ Remorse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remorse
+//
+//  Created by Ajioka Fumito on 2020/02/29.
+//  Copyright © 2020 Ajioka Fumito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
